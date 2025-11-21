3c101a8ff274029539409c18f673c9f86f9aef5a
Jesus Alberto Garcia <fairplaay16@gmail.com>
2025-11-21 00:14:37 +0000

    primer comitt

---
+++ eslint.config.js
@@ -0,0 +1,55 @@
+// eslint.config.js
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import pluginVue from 'eslint-plugin-vue';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default defineConfig([
+	{
+		name: 'app/files-to-lint',
+		files: ['**/*.{js,mjs,jsx,vue}'],
+	},
+
+	globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**', '**/node_modules/**']),
+
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+			},
+		},
+	},
+
+	js.configs.recommended,
+
+	...pluginVue.configs['flat/essential'],
+
+	// Desactiva reglas conflictivas con Prettier
+	prettierConfig,
+
+	// Activa Prettier como regla ESLint (Y MUY IMPORTANTE: especifica archivos)
+	{
+		files: ['**/*.{js,vue}'],
+		plugins: {
+			prettier: prettierPlugin,
+		},
+		rules: {
+			'prettier/prettier': [
+				'error',
+				{
+					printWidth: 100,
+					bracketSpacing: true,
+					arrowParens: 'avoid',
+					singleQuote: true,
+					useTabs: true, // 🔥 tú elegiste tabs: true
+					tabWidth: 4,
+					semi: true,
+					trailingComma: 'es5',
+					endOfLine: 'lf',
+				},
+			],
+		},
+	},
+]);
---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+	<h1>You did it!</h1>
+	<p>
+		Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+		{{ count }} / {{ doubleCount }}
+		documentation
+	</p>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			count: 42,
+		};
+	},
+	computed: {
+		doubleCount() {
+			this.count * 2;
+		},
+	},
+};
+</script>
+
+<style scoped></style>
